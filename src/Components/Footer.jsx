dcd4c3937f81c0ca71eb3dd693675cad5c51bc7a
Michael Taylor <mtaylor7193r@gmail.com>
2022-11-02 10:11:06 +0000

    Add Topics Component for each selected topic to be displayed. Added an api.js to use axios for paths and props

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,9 @@
 const Footer = () => {
-    return (
-        <h4>Copyright</h4>
+  return (
+    <div className="footer">
+      <h4>Copyright</h4>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Footer
+export default Footer;
